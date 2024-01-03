29590b6d8a0e2765fb47c7129b8d26ed3cd2d632
muadpn <muadpn434@gmail.com>
2024-01-03 20:28:33 +0530

    Fixed: Deployed BaseUrl to Domain for trpc provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,26 +4,15 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
-function getBaseUrl() {
-  if (typeof window !== "undefined")
-    // browser should use relative path
-    return "";
-  if (process.env.NEXT_PUBLIC_SERVER_URL)
-    // reference for vercel.com
-    return `https://${process.env.NEXT_PUBLIC_SERVER_URL}`;
-  //   if (process.env.RENDER_INTERNAL_HOSTNAME)
-  //     // reference for render.com
-  //     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+import { absoluteUrl } from "@/lib/utils";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          url: absoluteUrl("/api/trpc"),
         }),
       ],
     })
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
-  if (process.env.DOMAIN) return `https://${process.env.DOMAIN}${path}`;
+  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
